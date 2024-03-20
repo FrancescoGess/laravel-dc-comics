37c3f769bfbb1ab4be7965c81fcf3d684255e6ef
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-03-20 17:28:57 +0100

    creo tabella con i relativi dati dinamici

--- resources/views/pages/comics/index.blade.php
+++ resources/views/pages/comics/index.blade.php
@@ -6,6 +6,48 @@
     <h1>
         ciao
     </h1>
+
+   
+    <div
+        class="table-responsive"
+    >
+        <table
+            class="table table-primary"
+        >
+            <thead>
+                <tr>
+                    <th scope="col">title</th>
+                    <th scope="col">description</th>
+                    <th scope="col">thumb</th>
+                    <th scope="col">price</th>
+                    <th scope="col">series</th>
+                    <th scope="col">sale_date</th>
+                    <th scope="col">type</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $item)
+                <tr class="">
+                    <td scope="row">{{$item->title}}</td>
+                    <td>{{$item->description}}</td>
+                    <td>{{$item->thumb}}</td>
+                    <td>{{$item->price}}</td>
+                    <td>{{$item->series}}</td>
+                    <td>{{$item->sale_date}}</td>
+                    <td>{{$item->type}}</td>
+                </tr>
+                @endforeach
+                <tr class="">
+                    <td scope="row">Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+        
+    
 </main>
     
 @endsection
